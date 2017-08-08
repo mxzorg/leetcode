14bc50e31c99eac4680c4356d9e0c0280a3898c1
zhongyafeng <vincent_gemini@163.com>
2017-08-09 07:17:12 +0800

    add leetcode project file

---
+++ LeetCode/LeetCode/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LeetCode
+//
+//  Created by Gemini_Mxz on 2017/8/9.
+//  Copyright © 2017年 Mxz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LeetCodeSwift/LeetCodeSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LeetCodeSwift
+//
+//  Created by Gemini_Mxz on 2017/8/9.
+//  Copyright © 2017年 Mxz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
