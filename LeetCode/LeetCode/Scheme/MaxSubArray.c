7434e87ac2b3b10195ece93853f3c0be1408cc88
zhongyafeng <vincent_gemini@163.com>
2018-04-06 14:20:42 +0800

    modify 53 problem

--- LeetCode/LeetCode/Scheme/MaxSubArray.c
+++ LeetCode/LeetCode/Scheme/MaxSubArray.c
@@ -18,13 +18,8 @@
  如果右终点在左起点的左侧，则原计算结果为最大和，
  否则比较两次计算的终点最大和。
  
- 思考2:
- 与前一个一样，先要确定最优起点，然后求出最优终点。
- 不同的是，最优起点不是一次确定下来的，而是经过不同的比较来确定的，
- 当最大值比某个数字小时，最优起点一定是这个最大值；
- 当
+ 思考2:参考注释思路
  */
-
 
 int maxSubArray(int* nums, int numsSize) {
     
@@ -33,36 +28,24 @@ int maxSubArray(int* nums, int numsSize) {
     int sum = nums[0];
     for(int i = 1; i < numsSize; i++){
         
+        //累加求和
         sum += nums[i];
         if (sum > maxSum) {
+            
+            //记录增量
             maxSum = sum;
         }
         
+        //如果累计和比单个元素小，则重新累计
         if (sum < nums[i]) {
             sum = nums[i];
         }
+        
+        //如果最大值比单个元素小，则重置最大值，并且重新累计
         if (maxSum < nums[i]) {
             maxSum = nums[i];
             sum = nums[i];
         }
-        
-        
-//        if (sum > maxSum) {
-//            //重新计算最优起点
-//            if (sum < nums[i]) {
-//                maxSum = nums[i];
-//                sum = maxSum;
-//            }else{
-//                maxSum = sum;
-//            }
-//        } else if (maxSum < nums[i]) {
-//            //重新计算最优起点
-//            sum = nums[i];
-//            maxSum = nums[i];
-//        } else if (sum < nums[i]) {
-//            //重新计算最优起点
-//            sum = nums[i];
-//        }
     }
     
     return maxSum;
